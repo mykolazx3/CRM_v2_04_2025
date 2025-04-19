b2dab0007d04871b0e57861b801bb8138a8fe335
Mykola <mykolamalovanchuk@gmail.com>
2025-04-19 13:35:15 +0300

    -

--- Dockerfile
+++ Dockerfile
@@ -4,3 +4,7 @@ VOLUME /tmp
 ARG JAR_FILE=target/*.jar
 COPY ${JAR_FILE} app.jar
 ENTRYPOINT ["java", "-jar", "/app.jar"]
+
+#./mvnw clean package -DskipTests
+#mvn clean package -DskipTests
+#docker-compose up --build
--- src/test/java/com/mykola/crm/CrmV2042025ApplicationTests.java
+++ src/test/java/com/mykola/crm/CrmV2042025ApplicationTests.java
@@ -5,6 +5,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class CrmV2042025ApplicationTests {
 
     @Test
